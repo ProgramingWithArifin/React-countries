04be35c6b6f5d0984fbb74ab90a335345cafce74
Arifin Mozumder <100036799+ProgramingWithArifin@users.noreply.github.com>
2026-03-31 17:52:06 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Suspense } from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
 import Countries from './components/Countries/Countries'
 
 const countriesPromise = fetch("https://openapi.programming-hero.com/api/all").then(res=>res.json());
